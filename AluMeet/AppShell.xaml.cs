e3f5a2c906ec127bd75398310e26d9a8eca83c7b
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-06-12 21:15:36 -0400

    Initialize new .net maui project

---
+++ AluMeet/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AluMeet;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ AluMeet/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AluMeet;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
