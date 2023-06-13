a820f80844634111b4e42f3a8dfc14ab75db907a
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-06-12 22:47:43 -0400

    Added Model Class AlumniModel  The AlumniModel class represents an individual alumni entity in the application and serves as a blueprint for storing and retrieving information related to alumni. The class includes several properties, each with a detailed description.  'Id' represents the unique identifier for alumni and its type is int  'Name' stores the name of alumni  'GraduationYear' stores the date of graduation  'ContactDetails' stores the contact information of the alumni  'FacebookHandle' stores the facebook profile url  'LinkedInHandle' stores the info of linked in url  'TwitterHandle' stores the twitter profile url  'Email' stores the email  'CurrentPosition' stores the position of alumni the company  'CurrentEmployer' stores the current employer name  ‘Profile’Picture stores the image     ‘ProgramStudied’ stores the course alumni studied

---
+++ AluMeet/Model/AlumniModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace AluMeet.Model;
+
+
+/// <summary>
+/// The AlumniModel class represents an individual alumni entity in the application.
+/// It contains properties to store and retrieve information such as the alumni's ID, name,
+/// graduation year, contact details, twitter profile, facebook profile, linkedIn profile,
+/// email, current employer, profile picture, and current position.
+/// </summary>
+public class AlumniModel
+{
+    /// <summary>
+    /// Gets or sets the ID of the alumni.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the ProgramStudied of the alumni.
+    /// </summary>
+    public string ProgramStudied { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the alumni.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the graduation year of the alumni.
+    /// </summary>
+    public int GraduationYear { get; set; }
+
+    /// <summary>
+    /// Gets or sets the contact details of the alumni.
+    /// </summary>
+    public string ContactDetails { get; set; }
+
+    /// <summary>
+    /// Gets or sets the current employer of the alumni.
+    /// </summary>
+    public string CurrentEmployer { get; set; }
+
+    /// <summary>
+    /// Gets or sets the current position of the alumni.
+    /// </summary>
+    public string CurrentPosition { get; set; }
+
+    /// <summary>
+    /// Gets or sets the email of the alumni.
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    /// Gets or sets the twitter handle of the alumni.
+    /// </summary>
+    public string TwitterHandle { get; set; }
+
+    /// <summary>
+    /// Gets or sets the facebook profile url of the alumni.
+    /// </summary>
+    public string FacebookHandle { get; set; }
+
+    /// <summary>
+    /// Gets or sets the linkedIn profile url of the alumni.
+    /// </summary>
+    public string LinkedInHandle { get; set; }
+
+    /// <summary>
+    /// Gets or sets the profile picture of the alumni.
+    /// </summary>
+    public string ProfilePicture { get; set; }
+
+}
