5fc934439b142dcf8da31cbc1ee82a9b6cc3bbfe
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-08-03 20:10:53 -0400

    Add JobViewModel with Firebase Realtime Database Integration
    The JobViewModel handles user input for job-related information and contains method to save the job data to the firebase realtime database under "jobs" node.

--- AluMeet/ViewModels/RegisterViewModel.cs
+++ AluMeet/ViewModels/RegisterViewModel.cs
@@ -88,23 +88,24 @@ namespace AluMeet.ViewModels
 
         private async void RegisterBtnTappedAsync(object obj)
         {
-            try
-            {
-                // Connect our application to firebase auth
-                FirebaseAuthProvider firebaseAuthProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIKey));
-                // Firebase auth selected createUserWithEmailAndPassword option
-                var auth = await firebaseAuthProvider.CreateUserWithEmailAndPasswordAsync(Email, Password);
-                // after register firebase give us token
-                string token = auth.FirebaseToken;
-                // check token if it is null give alert message else back to login page
+            await SaveUserDataToDatabaseAsync();
+            //try
+            //{
+            //    // Connect our application to firebase auth
+            //    FirebaseAuthProvider firebaseAuthProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIKey));
+            //    // Firebase auth selected createUserWithEmailAndPassword option
+            //    var auth = await firebaseAuthProvider.CreateUserWithEmailAndPasswordAsync(Email, Password);
+            //    // after register firebase give us token
+            //    string token = auth.FirebaseToken;
+            //    // check token if it is null give alert message else back to login page
 
                 
-                if (token != null)
-                {
-                    await App.Current.MainPage.DisplayAlert("Alert", "User Registered successfully", "OK");
-                    SaveUserDataToDatabaseAsync();
-                    await this._navigation.PopAsync();
-                }
+            //    if (token != null)
+            //    {
+            //        await App.Current.MainPage.DisplayAlert("Alert", "User Registered successfully", "OK");
+                    
+            //        await this._navigation.PopAsync();
+            //    }
                 
                 
 
@@ -112,29 +113,29 @@ namespace AluMeet.ViewModels
 
 
 
-            }
-            catch (FirebaseAuthException ex)
-            {
-                if (ex.Reason == AuthErrorReason.EmailExists)
-                {
-                    await App.Current.MainPage.DisplayAlert("Error", "Email already exists. Please choose a different email.", "OK");
-                }
-                else if (ex.Reason == AuthErrorReason.InvalidEmailAddress)
-                {
-                    await App.Current.MainPage.DisplayAlert("Error", "Invalid email address. Please enter a valid email.", "OK");
-                }
-                else if (ex.Reason == AuthErrorReason.WeakPassword)
-                {
-                    await App.Current.MainPage.DisplayAlert("Error", "Weak password. Please choose a stronger password.", "OK");
-                }
-                else
-                {
-                    await App.Current.MainPage.DisplayAlert("Error", "Registration failed. Please try again.", "OK");
-                }
-            }
+            //}
+            //catch (FirebaseAuthException ex)
+            //{
+            //    if (ex.Reason == AuthErrorReason.EmailExists)
+            //    {
+            //        await App.Current.MainPage.DisplayAlert("Error", "Email already exists. Please choose a different email.", "OK");
+            //    }
+            //    else if (ex.Reason == AuthErrorReason.InvalidEmailAddress)
+            //    {
+            //        await App.Current.MainPage.DisplayAlert("Error", "Invalid email address. Please enter a valid email.", "OK");
+            //    }
+            //    else if (ex.Reason == AuthErrorReason.WeakPassword)
+            //    {
+            //        await App.Current.MainPage.DisplayAlert("Error", "Weak password. Please choose a stronger password.", "OK");
+            //    }
+            //    else
+            //    {
+            //        await App.Current.MainPage.DisplayAlert("Error", "Registration failed. Please try again.", "OK");
+            //    }
+            //}
         }
 
-        private void  SaveUserDataToDatabaseAsync()
+        private async Task  SaveUserDataToDatabaseAsync()
         {
             // Firebase Realtime Database URL
             string firebaseDatabaseUrl = "https://alummeet-af9e0-default-rtdb.firebaseio.com/";
@@ -145,12 +146,13 @@ namespace AluMeet.ViewModels
        
 
             // Save user data to Firebase Realtime Database under "Alumni" node
-            var alumni = firebaseClient.Child("Alumni").PostAsync(new AlumniModel
+            var alumni = await firebaseClient.Child("Alumni").PostAsync(new AlumniModel
             {
                 Name = FullName,
                 Email = Email,
                 GraduationYear = GraduationYear
             });
+            string s = "s";
         }
 
         public string WebAPIKey = "AIzaSyCkRo_giGFKNA04W7NArjMPXeHHpKnooAo";
