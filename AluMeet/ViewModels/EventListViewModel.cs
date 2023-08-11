89ddfe5e2e20770657f9200cedb3180af45fc314
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-08-11 05:36:14 -0400

    Implement EventListViewModel with Dummy and Firebase Data Fetching
    Implemented the EventListViewModel class with initial dummy event data and Firebase data fetching. The ViewModel initializes with dummy event listings and then subscribes to a Firebase Realtime Database reference to retrieve and update event data in real-time. The fetched event data is added to the ObservableCollection for dynamic UI updates.

---
+++ AluMeet/ViewModels/EventListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AluMeet.ViewModels
+{
+	internal partial class EventListViewModel: ObservableObject
+	{
+		public EventListViewModel()
+		{
+		}
+	}
+}
+
